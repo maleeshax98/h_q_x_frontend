0c91cef7dc3280252d7108439f994ebeb6259692
MaleeshaX <142221263+maleeshax98@users.noreply.github.com>
2023-09-17 14:10:59 +0530

    Update useLogin.js

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,6 +1,7 @@
 import  { useState } from 'react'
 import axios from "axios"
 import useAuthContext from './useAuthContext'
+import OneSignal from 'react-onesignal';
 
 function useLogin() {
     const [isPending, setIsPending] = useState(false)
@@ -13,7 +14,8 @@ function useLogin() {
         setIsPending(true)
 
         try{
-            
+            await OneSignal.init({ appId: 'c07c00f1-6f5c-4f0b-bf4e-6114fb77de76'});
+            OneSignal.Slidedown.promptPush();
             const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/user/login`, {
                 email,
                 password
